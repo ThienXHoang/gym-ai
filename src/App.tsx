d2f2ebe2bea1939d8cd2fa41d453d471209e97d6
Thien Hoang <thienxhoang.dev@gmail.com>
2026-03-21 20:05:51 -0500

    feat: navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import Onboarding from "./pages/Onboarding.tsx";
 import Profile from "./pages/Profile.tsx";
 import Auth from "./pages/Auth.tsx";
 import Account from "./pages/Account.tsx";
+import Navbar from "./components/Navbar.tsx";
 
 function App() {
   return (
       <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/onboarding" element={<Onboarding />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/auth/:pathname" element={<Auth />} />
-          <Route path="/account/:pathname" element={<Account />} />
-        </Routes>
+          <div className="min-h-screen flex flex-col">
+              <main className="flex-1">
+                  <Navbar />
+                  <Routes>
+                      <Route index element={<Home />} />
+                      <Route path="/onboarding" element={<Onboarding />} />
+                      <Route path="/profile" element={<Profile />} />
+                      <Route path="/auth/:pathname" element={<Auth />} />
+                      <Route path="/account/:pathname" element={<Account />} />
+                  </Routes>
+              </main>
+          </div>
       </BrowserRouter>
   )
 }
